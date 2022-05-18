63a1fcd58c61a9b3a34ccd327ad2c64f6fbdf8f1
Tatiana Turin <tatiana.turin@gmail.com>
2022-05-18 08:27:24 -0400

    commit -2

--- src/test.java
+++ src/test.java
@@ -6,6 +6,14 @@ public class test {
         }
         return sum;
     }
+    private static int recursion_1(int n){
+        if(n==1){
+            return 1;
+        }else{
+            return n+recursion_1(n-1);
+        }
+
+    }
 
     private static  int factorial(int n){
         int ans = 1;
@@ -15,6 +23,13 @@ public class test {
         return ans;
     }
 
+    private static int recursion_2(int n){
+        if(n==1){
+            return 1;
+        }else {
+            return n*recursion_2(n-1);
+        }
+    }
     private static int n_to_m(int n, int m){
         int ans = 1;
         for(int i=1; i<=m;i++){
@@ -23,20 +38,41 @@ public class test {
         return ans;
     }
 
-    private static int fib1(int n){
+    private static int recursion_3(int n, int m){
+        if(m==1){
+            return n;
+        }else{
+            return n*recursion_3(n,m-1);
+        }
+    }
 
-huj
+
+
+
+
+    private static int fib1(int n){
+        if(n==0){
+            return 0;
+        }else if (n==1){
+            return 1;
+        }else {
+            return fib1(n-1)+fib1(n-2);
+        }
+
     }
     public static void main(String[] args) {
-        int ans = sum_natural_num(3);
-       // System.out.println(ans);
+        int ans = sum_natural_num(4);
+
+       // ans = recursion_3(2,8);
+        System.out.println(ans);
 
         ans = factorial(10);
-        System.out.println(ans);
+     //   System.out.println(ans);
 
         ans = n_to_m(2,8);
-        System.out.println(ans);
+      //  System.out.println(ans);
 
-
+        ans = fib1(4);
+     //   System.out.println(ans);
     }
 }
